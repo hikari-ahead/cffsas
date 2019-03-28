af708aad717dd19a05339ad774c68cfd4760925f
Kuangjeon Tian <tianguangqian@bytedance.com>
2019-03-28 16:15:24 +0800

    Initial Commit

---
+++ cffsas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cffsas
+//
+//  Created by resober on 2019/3/28.
+//  Copyright © 2019 resober. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
